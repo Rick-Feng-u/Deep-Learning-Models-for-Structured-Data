c3a78d973885c546b901272d6e1f1b11683d61c1
Rick Feng <rickfeng99@gmail.com>
2022-03-29 12:58:34 -0700

    parsing xml script completed

--- Script/parsing_wiki_xml_dump/parse_wiki_xml.py
+++ Script/parsing_wiki_xml_dump/parse_wiki_xml.py
@@ -3,33 +3,44 @@ import xml.etree.cElementTree as ET
 import time
 import re
 import os
+import gc
 
 start_time = time.time()
 
+
+def isEnglish(s):
+    try:
+        s.encode(encoding='utf-8').decode('ascii')
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
+
+
 def hms_string(sec_elapsed):
-    
     h = int(sec_elapsed / (60 * 60))
     m = int((sec_elapsed % (60 * 60)) / 60)
     s = sec_elapsed % 60
     return "{}:{:>02}:{:>05.2f}".format(h, m, s)
 
+
 def tag_name(t):
-    
     idx = k = t.rfind("}")
     if idx != -1:
         t = t[idx + 1:]
     return t
 
+
 def indent(elem, level=0):
-    i = "\n" + level*"  "
-    j = "\n" + (level-1)*"  "
+    i = "\n" + level * "  "
+    j = "\n" + (level - 1) * "  "
     if len(elem):
         if not elem.text or not elem.text.strip():
             elem.text = i + "  "
         if not elem.tail or not elem.tail.strip():
             elem.tail = i
         for subelem in elem:
-            indent(subelem, level+1)
+            indent(subelem, level + 1)
         if not elem.tail or not elem.tail.strip():
             elem.tail = j
     else:
@@ -37,67 +48,122 @@ def indent(elem, level=0):
             elem.tail = j
     return elem
 
-def WikiReader(FILE_PATH, OUTPUT_PATH):
+
+def WikiReader(TITLE_PATH, FILE_PATH, OUTPUT_PATH):
     totalCount = 0
     title = None
-    
-    INPUT_PATH = os.path.join(FILE_PATH, 'enwiki-20220201-pages-articles-multistream1.xml')
-    
+    gc.enable()
+
+    INPUT_PATH = os.path.join(FILE_PATH, 'enwiki-20220201-pages-articles-multistream.xml')
+
     for event, elem in etree.iterparse(INPUT_PATH, events=('start', 'end')):
+        if totalCount > 1 and (totalCount % 100000) == 0:
+            print("{:,}".format(totalCount))
         tage = tag_name(elem.tag)
-        
+
         if event == 'start':
             if tage == 'page':
                 title = ''
                 redirect = False
                 text = ''
         else:
-            if tage == 'title':
+            if (tage == 'title'):
                 title = elem.text
-                title_ = ''.join(filter(str.isalnum, title)) 
-                root = ET.Element(title_)
+                store = title
+                title = re.sub('[^a-zA-Z\d\s]', '', title)
+                title = re.sub('\s+', '_', title)
+                try:
+                    if title[0].isdigit():
+                        title = "_" + title
+
+                except:
+                    continue
+                root = ET.Element(title)
+
             elif tage == 'redirect':
                 redirect = True
             elif tage == 'text':
                 text = elem.text
-                
+                if text == "":
+                    continue
+
                 REDIRECT_CASE_1 = "#REDIRECT"
                 REDIRECT_CASE_2 = "#redirect"
-                if (REDIRECT_CASE_1 in text) | (redirect == True) | (REDIRECT_CASE_2 in text):
-                    continue 
-                
-                for line in text.splitlines():
-                    special_char=re.compile('[@_!$%^&*()<>?/\|}{~:;]#')
-                    span = re.compile('span')
-                    if line.startswith("==") & line.endswith('==') & (special_char.search(line) == None) & (span.search(line) == None):
-                        if(line.count("=") == 4):
-                            current_sub_1_title =  line[line.find('==') + len('=='):line.rfind('==')]
-                            sub_title = ET.SubElement(root, current_sub_1_title)
-                        elif(line.count("=") == 6):
-                            current_sub_2_title =  line[line.find('===') + len('==='):line.rfind('===')]
-                            sub_2_title = ET.SubElement(sub_title, current_sub_2_title)
-                        elif(line.count("=") == 8):
-                            current_sub_3_title =  line[line.find('====')+1:line.find('====')]
-                            sub_3_title = ET.SubElement(sub_2_title, current_sub_3_title)
-                        elif(line.count("=") == 10):
-                            current_sub_4_title =  line[line.find('=====')+1:line.find('=====')]
-                            sub_4_title = ET.SubElement(sub_3_title, current_sub_4_title)
-                        elif(line.count("=") == 12):
-                            current_sub_5_title =  line[line.find('======')+1:line.find('======')]
-                            sub_5_title = ET.SubElement(sub_4_title, current_sub_5_title)
-                
-                tree = ET.ElementTree(indent(root))
-                xml_path = os.path.join(OUTPUT_PATH, title_ + ".xml")
-                tree.write(xml_path, xml_declaration=True, encoding='utf-8')
-                      
-            elif tage == 'page':
-                totalCount += 1     
+                try:
+                    if (REDIRECT_CASE_1 in text) | (redirect == True) | (REDIRECT_CASE_2 in text):
+                        continue
+                except:
+                    continue
 
-                if totalCount > 1 and (totalCount % 100000) == 0:
-                    print("{:,}".format(totalCount))
-                
+                try:
+                    for line in text.splitlines():
+                        special_char = re.compile('[@#*()<>/\|}{~]')
+                        span = re.compile('span')
+                        if line.startswith("==") & line.endswith('==') & (special_char.search(line) == None) & (
+                                span.search(line) == None) & (
+                                any(c.isalpha() for c in line) | any(c.isdigit() for c in line)) & (isEnglish(line)):
+                            if (line.count("=") == 4) & (line.isascii() == True):
+                                current_sub_1_title = line[line.find('==') + len('=='):line.rfind('==')]
+                                if current_sub_1_title != '':
+                                    current_sub_1_title = re.sub('[^a-zA-Z\d\s]', '', current_sub_1_title)
+                                    current_sub_1_title = re.sub('\s+', '_', current_sub_1_title)
+                                    if current_sub_1_title[0].isdigit():
+                                        current_sub_1_title = "_" + current_sub_1_title
+                                    sub_title = ET.SubElement(root, current_sub_1_title)
+                            elif (line.count("=") == 6):
+                                current_sub_2_title = line[line.find('===') + len('==='):line.rfind('===')]
+                                if current_sub_2_title != '':
+                                    current_sub_2_title = re.sub('[^a-zA-Z\d\s]', '', current_sub_2_title)
+                                    current_sub_2_title = re.sub('\s+', '_', current_sub_2_title)
+                                    if current_sub_2_title[0].isdigit():
+                                        current_sub_2_title = "_" + current_sub_2_title
+                                    sub_2_title = ET.SubElement(sub_title, current_sub_2_title)
+                            elif (line.count("=") == 8):
+                                current_sub_3_title = line[line.find('====') + len('===='):line.rfind('====')]
+                                if current_sub_3_title != '':
+                                    current_sub_3_title = re.sub('[^a-zA-Z\d\s]', '', current_sub_3_title)
+                                    current_sub_3_title = re.sub('\s+', '_', current_sub_3_title)
+                                    if current_sub_3_title[0].isdigit():
+                                        current_sub_3_title = "_" + current_sub_3_title
+                                    sub_3_title = ET.SubElement(sub_2_title, current_sub_3_title)
+                            elif (line.count("=") == 10):
+                                current_sub_4_title = line[line.find('=====') + len('====='):line.rfind('=====')]
+                                if current_sub_4_title != '':
+                                    current_sub_4_title = re.sub('[^a-zA-Z\d\s]', '', current_sub_4_title)
+                                    current_sub_4_title = re.sub('\s+', '_', current_sub_4_title)
+                                    if current_sub_4_title[0].isdigit():
+                                        current_sub_4_title = "_" + current_sub_4_title
+                                    sub_4_title = ET.SubElement(sub_3_title, current_sub_4_title)
+                            elif (line.count("=") == 12):
+                                current_sub_5_title = line[line.find('======') + len('======'):line.rfind('======')]
+                                if current_sub_5_title != '':
+                                    current_sub_5_title = re.sub('[^a-zA-Z\d\s]', '', current_sub_5_title)
+                                    current_sub_5_title = re.sub('\s+', '_', current_sub_5_title)
+                                    if current_sub_5_title[0].isdigit():
+                                        current_sub_5_title = "_" + current_sub_5_title
+                                    sub_5_title = ET.SubElement(sub_4_title, current_sub_5_title)
+
+                except:
+                    continue
+
+                try:
+                    tree = ET.ElementTree(indent(root))
+                    xml_path = os.path.join(OUTPUT_PATH, title + ".xml")
+                    tree.write(xml_path, xml_declaration=True, encoding='utf-8')
+
+                    with open(TITLE_PATH, "a+", encoding="utf-8") as file_object:
+                        file_object.write(store + "   "+ title)
+                        file_object.write("\n")
+
+                except:
+                    continue
+
+            elif tage == 'page':
+                totalCount += 1
+
+
             elem.clear()
-                
+
+
 elapsed_time = time.time() - start_time
 print("Elapsed time: {}".format(hms_string(elapsed_time)))
-
--- xml_to_seq.py
+++ xml_to_seq.py
@@ -1,7 +1,39 @@
-from Util import TESTING_XML_FOLDER_PATH
-from Script import xml_to_prufer
 import os
+import torch
+import pickle
+from Util import CLEANED_WIKI_XML_FOLDER_PATH, sequence, sequence_to_index
+from Script import xml_to_prufer
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 if __name__ == '__main__':
-    FILE_PATH = os.path.join(TESTING_XML_FOLDER_PATH, 'test.xml')
-    resulting_sequnece = xml_to_prufer(FILE_PATH)
+    pairs = []
+
+    input_seq_name = sequence('wiki_input')
+    out_seq_name = sequence('wiki_output')
+
+    for file in os.listdir(CLEANED_WIKI_XML_FOLDER_PATH):
+        filename = os.fsdecode(file)
+        if filename.endswith(".xml"):
+            FILE_PATH = os.path.join(CLEANED_WIKI_XML_FOLDER_PATH, filename)
+            result = xml_to_prufer(FILE_PATH)
+
+            if result is None:
+                continue
+
+            pair = []
+
+            target_seq = ["war"]
+            input_seq = result
+
+            input_seq_name.add_sequence(input_seq)
+            out_seq_name.add_sequence(target_seq)
+
+            input_seq = sequence_to_index(input_seq_name, input_seq)
+            target_seq = sequence_to_index(out_seq_name, target_seq)
+
+            pair.append(input_seq)
+            pair.append(target_seq)
+
+            pairs.append(pair)
+
+    # print(random.choice(pairs))
