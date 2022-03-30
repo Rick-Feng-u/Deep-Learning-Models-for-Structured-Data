d97789cc67e59d7fbbb3359fb359038452b84a23
Rick Feng <rickfeng99@gmail.com>
2022-03-29 19:37:11 -0700

    updated const file, now it include pickel file folder

--- Util/const.py
+++ Util/const.py
@@ -4,6 +4,7 @@ WIKI_XML_DUMP_FOLDER_PATH = os.path.join('Data', 'raw_data', 'wiki_xml_dump')
 CLEANED_TITLE_FOLDER_PATH = os.path.join('Data', 'cleaned_data', 'cleaned_title')
 
 CLEANED_WIKI_XML_FOLDER_PATH = os.path.join('Data', 'cleaned_data', 'cleaned_wiki_folder')
+Cleaned_WIKI_SEQ_FOLDER = os.path.join('Data', 'cleaned_data', 'cleaned_wiki_seq_folder')
 WIKI_CLEANED_TITLES_FILE_PATH = os.path.join(CLEANED_TITLE_FOLDER_PATH, 'wiki_title.txt')
 WIKI_CLEANED_TITLES_ID_FILE_PATH = os.path.join(CLEANED_TITLE_FOLDER_PATH, 'wiki_title_with_id.txt')
 WIKI_CLEANED_TITLES_TARGET_FILE_PATH = os.path.join(CLEANED_TITLE_FOLDER_PATH, 'wiki_title_with_target.txt')
